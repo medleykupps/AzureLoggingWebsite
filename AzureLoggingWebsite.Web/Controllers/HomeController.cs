ed9936ee8a5265f9ac0a1cc1cdc4556b7dacefa0
sam.medley <medleykupps@outlook.com>
2015-11-21 15:10:21 +1100

    Initial commit
    - Enable and test tracing in Output window of Visual Studio
    - Test tracing in trace.axd endpoint

---
+++ AzureLoggingWebsite.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureLoggingWebsite.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Homepage()
+        {
+            Trace.WriteLine("Entering Homepage action");
+
+            // Output an Information level message
+            Trace.TraceInformation("Generate Information trace at {0}", DateTime.Now);
+
+            // Output a Warning level message
+            Trace.TraceWarning("Generate Warning trace at {0}", DateTime.Now);
+
+            // Output an Error level message
+            Trace.TraceError("Generate Error trace at {0}", DateTime.Now);
+
+            return View();
+        }
+    }
+}
